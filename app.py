6a9ee96785450b993139309c580019119b647eef
Youx <juan.ferreira-youxlab@institutoyoux.org.br>
2025-09-04 19:15:40 -0300

    escrevendo o codigo com minhas variaveis pessoais

---
+++ app.py
@@ -0,0 +1,5 @@
+nome = ('Juan')
+idade = int('16')
+linguafav = ('python')
+print ('meu nome é {}, tenho {} anos de idade, minha liguagem de programação' \
+' favorita é {}' .format (nome, idade, linguafav, ))
